d1979af28db769aeda909cddba1a374a6b2cc246
Phuwanai Thummavet <mr.thummavet@gmail.com>
2018-08-01 15:08:31 +0700

    update run/main.js

--- run/main.js
+++ run/main.js
@@ -13,12 +13,7 @@ var Web3               = require('web3'),
     PizzaCoinPlayerJson = require('../build/contracts/PizzaCoinPlayer.json'),
     PizzaCoinTeamJson = require('../build/contracts/PizzaCoinTeam.json');
 
-/*
-        let web3 = new Web3.providers.HttpProvider('http://localhost:7545')
-    
-        The above code works with web3@0.20.2 but it does not work with web3@1.0.0,
-        since web3@1.0.0 does no longer support for HttpProvider() anymore
-    */
+
 var web3 = new Web3('http://localhost:7545');
 //var web3 = new Web3('http://localhost:8545');
 
@@ -50,7 +45,7 @@ async function main() {
         staffContractAddr, 
         playerContractAddr, 
         teamContractAddr
-    ] = await initContracts(ethAccounts);
+    ] = await initContracts(ethAccounts[0]);
 
     console.log('\nInitializing contracts succeeded...');
     console.log('PizzaCoin address: ' + PizzaCoinJson.networks[5777].address);
@@ -60,30 +55,92 @@ async function main() {
     console.log('PizzaCoinTeam address: ' + teamContractAddr);
 
     // Register a staff
-    registerStaff(ethAccounts, ethAccounts[1], 'bright');
+    registerStaff(ethAccounts[0], ethAccounts[1], 'bright');
     console.log('\nRegistering a staff succeeded...');
 
     // Register a staff
-    registerStaff(ethAccounts, ethAccounts[2], 'bright');
+    registerStaff(ethAccounts[0], ethAccounts[2], 'bright');
     console.log('\nRegistering a staff succeeded...');
 
     // Kick a staff
-    kickStaff(ethAccounts, ethAccounts[2]);
+    kickStaff(ethAccounts[0], ethAccounts[2]);
     console.log('\nKicking a staff succeeded...');
 
     // Register a staff
-    registerStaff(ethAccounts, ethAccounts[2], 'bright');
+    registerStaff(ethAccounts[0], ethAccounts[2], 'bright');
     console.log('\nRegistering a staff succeeded...');
+
+    // Create a team
+    createTeam(ethAccounts[3], 'serial-coder', 'pizza');
+    console.log('\nCreating a new team succeeded...');
+
+    // Register a player
+    registerPlayer(ethAccounts[4], 'bright', 'pizza');
+    console.log('\nRegistering a player succeeded...');
+
+    // Register a player
+    registerPlayer(ethAccounts[5], 'bright', 'pizza');
+    console.log('\nRegistering a player succeeded...');
+
+    // Create a team
+    createTeam(ethAccounts[6], 'robert', 'pizzaHack');
+    console.log('\nCreating a new team succeeded...');
+
+    // Register a player
+    registerPlayer(ethAccounts[7], 'bob', 'pizzaHack');
+    console.log('\nRegistering a player succeeded...');
+
+
+
+
+
+    // kick player, team, staff (by staff, project deployer (kick himself too), normal player)
+    // vote (by staff, player)
+    // call functions above in other different contract states
+    // show results, ...
 }
 
-async function registerStaff(ethAccounts, staffAddr, staffName) {
+async function registerPlayer(playerAddr, playerName, teamName) {
     let err, receipt;
-    let staffAddrRet, staffNameRet;
+
+    // Register a player
+    [err, receipt] = await callContractFunction(
+        PizzaCoin.methods.registerPlayer(playerName, teamName).send({
+            from: playerAddr,
+            gas: 6500000,
+            gasPrice: 10000000000
+        })
+    );
+
+    if (err) {
+        throw new Error(err);
+    }
+}
+
+async function createTeam(creatorAddr, creatorName, teamName) {
+    let err, receipt;
+
+    // Create a new team
+    [err, receipt] = await callContractFunction(
+        PizzaCoin.methods.createTeam(teamName, creatorName).send({
+            from: creatorAddr,
+            gas: 6500000,
+            gasPrice: 10000000000
+        })
+    );
+
+    if (err) {
+        throw new Error(err);
+    }
+}
+
+async function registerStaff(registrarAddr, staffAddr, staffName) {
+    let err, receipt;
 
     // Register a staff
     [err, receipt] = await callContractFunction(
         PizzaCoin.methods.registerStaff(staffAddr, staffName).send({
-            from: ethAccounts[0],
+            from: registrarAddr,
             gas: 6500000,
             gasPrice: 10000000000
         })
@@ -92,23 +149,15 @@ async function registerStaff(ethAccounts, staffAddr, staffName) {
     if (err) {
         throw new Error(err);
     }
-
-    /*staffAddrRet = receipt.events.StaffRegistered.returnValues._staff;
-    staffNameRet = receipt.events.StaffRegistered.returnValues._staffName;
-
-    if (staffAddr !== staffAddrRet || staffName !== staffNameRet) {
-        throw new Error("Registering a staff failed");
-    }*/
 }
 
-async function kickStaff(ethAccounts, staffAddr) {
+async function kickStaff(kickerAddr, staffAddr) {
     let err, receipt;
-    let staffAddrRet;
 
     // Kick a staff
     [err, receipt] = await callContractFunction(
         PizzaCoin.methods.kickStaff(staffAddr).send({
-            from: ethAccounts[0],
+            from: kickerAddr,
             gas: 6500000,
             gasPrice: 10000000000
         })
@@ -117,15 +166,9 @@ async function kickStaff(ethAccounts, staffAddr) {
     if (err) {
         throw new Error(err);
     }
-
-    /*staffAddrRet = receipt.events.StaffKicked.returnValues._staffToBeKicked;
-
-    if (staffAddr !== staffAddrRet) {
-        throw new Error("Kicking a staff failed");
-    }*/
 }
 
-async function initContracts(ethAccounts) {
+async function initContracts(projectDeployerAddr) {
     let err, receipt;
     let staffContractAddr, playerContractAddr, teamContractAddr;
     let state;
@@ -133,7 +176,7 @@ async function initContracts(ethAccounts) {
     // Create PizzaCoinStaff contract
     [err, receipt] = await callContractFunction(
         PizzaCoin.methods.createStaffContract().send({
-            from: ethAccounts[0],
+            from: projectDeployerAddr,
             gas: 6500000,
             gasPrice: 10000000000
         })
@@ -148,7 +191,7 @@ async function initContracts(ethAccounts) {
     // Create PizzaCoinPlayer contract
     [err, receipt] = await callContractFunction(
         PizzaCoin.methods.createPlayerContract().send({
-            from: ethAccounts[0],
+            from: projectDeployerAddr,
             gas: 6500000,
             gasPrice: 10000000000
         })
@@ -163,7 +206,7 @@ async function initContracts(ethAccounts) {
     // Create PizzaCoinTeam contract
     [err, receipt] = await callContractFunction(
         PizzaCoin.methods.createTeamContract().send({
-            from: ethAccounts[0],
+            from: projectDeployerAddr,
             gas: 6500000,
             gasPrice: 10000000000
         })
@@ -178,7 +221,7 @@ async function initContracts(ethAccounts) {
     // Change all contracts' state from Initial to Registration
     [err, receipt] = await callContractFunction(
         PizzaCoin.methods.startRegistration().send({
-            from: ethAccounts[0],
+            from: projectDeployerAddr,
             gas: 6500000,
             gasPrice: 10000000000
         })
@@ -191,7 +234,7 @@ async function initContracts(ethAccounts) {
     // Check the contracts' state
     [err, state] = await callContractFunction(
         PizzaCoin.methods.getContractState().call({
-            from: ethAccounts[0],
+            from: projectDeployerAddr,
         })
     );
 
