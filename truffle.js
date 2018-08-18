effce404a02ab3dc66c1bddb1a7071f07005bb42
Phuwanai Thummavet <mr.thummavet@gmail.com>
2018-08-18 16:04:14 +0700

    remove unused code in truffle.js

--- truffle.js
+++ truffle.js
@@ -10,26 +10,20 @@ module.exports = {
       provider: function() {
         return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/3ikLuZwohJ81nAe4aPyI');
       },
-      network_id: '3'/*,
-      gas: 7000000,
-      gasPrice: 10000000000*/
+      network_id: '3'
     },
     rinkeby: {
       provider: function() {
         //return new HDWalletProvider(mnemonic, 'http://localhost:8545');
         return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/3ikLuZwohJ81nAe4aPyI');
       },
-      network_id: '4'/*,
-      gas: 7000000,
-      gasPrice: 10000000000*/
+      network_id: '4'
     },
     rinkeby_localsync: {
       host: 'localhost',
       port: 8545,
       network_id: '4',
-      from: '0x4B8Ad23e5923c7F479F35615a05e5868325aA85B'/*,
-      gas: 5000000,   // 7500000
-      gasPrice: 10000000000*/
+      from: '0x4B8Ad23e5923c7F479F35615a05e5868325aA85B'
     },
     ganache: {
       host: 'localhost',
